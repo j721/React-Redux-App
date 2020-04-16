95e38248c455198a6eb8c66b2ca123572b18057d
j721 <juliadvu@gmail.com>
2020-04-15 20:31:48 -0500

    Changes to index.js file

--- app/src/index.js
+++ app/src/index.js
@@ -6,12 +6,11 @@ import App from './App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './store/reducers';
+import { reducer }from './store/reducers/reducer';
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
-const rootElement = document.getElementById('root');
 ReactDOM.render(
     <Provider store ={store}>
       <App />
@@ -20,7 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
